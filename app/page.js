5c0cf5aa34286763a3327a18426694689a3280ef
unknown <sujalagre26@gmail.com>
2024-08-10 03:32:37 +0530

    add github icon

--- app/page.js
+++ app/page.js
@@ -1,22 +1,18 @@
-"use client"
-import Link from "next/link";
-import DiscordStatus from "/components/DiscordStatus.js"
+'use client';
+import DiscordStatus from "@/components/DiscordStatus";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
-
   return (
-    <>
-
-      <div className="flex justify-center items-center min-h-screen" >
-        <Link href={"https://github.com/SujalAgre"} target="_blank">My Discord Status:</Link> 
-        <div className="w-4 mt-1 ml-1">
-           <DiscordStatus  />
-        </div>
-      </div>
-
-    </>
-
+    <div className="flex items-center justify-center min-h-screen">
+      <a href="https://github.com/SujalAgre/Discord-Status-Widget-in-Next.js" target="_blank" 
+        className="absolute top-4 left-4" >
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      
+       <p className="text-lg"> My Discord Status: </p>
+       <div className='w-4 ml-1 flex items-center'><DiscordStatus /></div>
+    </div>
   );
-
 }
-
